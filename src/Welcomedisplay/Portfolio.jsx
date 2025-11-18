44d610e8cf616ecba59f6bb9e1a92e65bae4c593
Azebi-Alex Steven .T. <tipo4542@gmail.com>
2025-11-18 21:56:39 +0100

    update

--- src/Welcomedisplay/Portfolio.jsx
+++ src/Welcomedisplay/Portfolio.jsx
@@ -3,6 +3,7 @@ import servicekimg from "../assets/Images/Screenshot 2025-03-24 230157.png";
 import tipobrokerimg from "../assets/Images/Screenshot 2025-03-24 230722.png";
 import portfolioimg from "../assets/Images/Screenshot 2025-03-24 231604.png";
 import EMRMSimg from "../assets/Images/Screenshot 2025-04-15 051447.png";
+import TicketsGigimg from "../assets/Images/Screenshot 2025-11-18 214030.png";
 const Portfolio = () => {
   return (
     <section id="projects">
@@ -98,6 +99,30 @@ const Portfolio = () => {
           <div className="mt-8">
             <Link
               to="https://med-records-management-system.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bg- hover:bg-darker-emerald-green font-medium outline-2 hover:outline-outline-z text-lg text-black hover:text-white p-4 rounded-2xl cursor-pointer transition-all duration-300 hover:-translate-y-3 focus:outline-white" data-aos="fade-up"
+            data-aos-delay="500">
+                View Project
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="px-8 mt-15">
+          <img src={TicketsGigimg} alt="" className="rounded-md mb-4" data-aos="zoom-out"
+            data-aos-delay="300" />
+          <h2 className=" text-xl font-primary font-medium" data-aos="fade-left"
+            data-aos-delay="1000">
+            Event Tickets Management System
+          </h2>
+          <p className="text-gray-300 text-md mt-4" data-aos="fade-up"
+            data-aos-delay="800">
+            I developed a secure ticket management system to streamline event ticketing processes. This full-stack system enables users to browse events, purchase tickets, and manage their bookings with features like real-time availability tracking and secure payment processing. Built with React.js and PocketBase, it offers role-based access, and responsive dashboards for both desktop and mobile use.
+          </p>
+          <div className="mt-8">
+            <Link
+              to="https://ticketsgig.vercel.app"
               target="_blank"
               rel="noopener noreferrer"
             >
